5a1ec32c5e28628e1754527c020f56aea66817d7
abd-2023 <abcoding17@gmail.com>
2024-07-02 14:11:13 +0530

    feat: Add Mobile Navigation Menu

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNavMobile() {
+	let navMobileContainer = document.getElementById('nav-mobile-container')
+	navMobileContainer.classList.toggle('show-nav-mobile-container')
+	// console.log('open nav menu', navMobileContainer.classList)
+}
+console.log('script load complete')
